380d44099fb92ca443689f42a7bb440061ee4975
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-08-26 07:24:13 +0530

    "init App.jsx"

--- React-10-Projects-project-4-starter/app/src/App.jsx
+++ React-10-Projects-project-4-starter/app/src/App.jsx
@@ -1,10 +1,22 @@
+import styled from 'styled-components'
 const App = () => {
   return (
-<mainContainer>
-  
-</mainContainer>
+<MainContainer>
+    <TopContainer>
+      <div className="logo">
+        <img src="/logo.svg" alt="logo" />
+      </div>
+    </TopContainer>
+</MainContainer>
 
   )
 };
 
 export default App;
+
+const MainContainer = styled.div`
+  
+`
+const TopContainer = styled.section`
+  
+`
