dc9f6f1f12c5ba96de46f8f6a35d9551147c3740
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-08-27 17:17:21 +0530

    "Pass data prop to SearchResult component and update its function signature to accept it"

--- React-10-Projects-project-4-starter/app/src/components/searchResult/SearchResult.jsx
+++ React-10-Projects-project-4-starter/app/src/components/searchResult/SearchResult.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from "styled-components";
 
 
-const SearchResult = () => {
+const SearchResult = ({data}) => {
   return (
     <FoodCardContainer>
     <FoodCard></FoodCard>
