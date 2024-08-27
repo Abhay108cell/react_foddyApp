473ef11221a2bd3905096dbd614eb067d366d44e
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-08-27 17:00:19 +0530

    "Formatted code, added error and loading states, and uncommented fetchFoodData function call"

--- React-10-Projects-project-4-starter/app/src/App.jsx
+++ React-10-Projects-project-4-starter/app/src/App.jsx
@@ -1,61 +1,60 @@
-import { useState } from 'react';
-import styled from 'styled-components'
+import { useState } from "react";
+import styled from "styled-components";
 
-const BASE_URL = "http://localhost:9000"
-
+const BASE_URL = "http://localhost:9000";
 
 const App = () => {
-  const [data, setData] = useState(null)
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(null)
-   setLoading(true)
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  const fetchFoodData =  async () =>{
+  const fetchFoodData = async () => {
+    setLoading(true);
     try {
-      const response = await fetch (BASE_URL)
-      const json =  await response.json();
-      
+      const response = await fetch(BASE_URL);
+      const json = await response.json();
+
       setData(json);
-      setLoading(false)
-
+      setLoading(false);
     } catch (error) {
-      setError("unable to fetch data")
+      setError("unable to fetch data");
     }
-  
-  }
+  };
 
-  fetchFoodData();
+  // fetchFoodData();
+
+  if(error) return <div>{error}</div>
+  if(loading) return <div>loading...</div>
 
   return (
-<Container>
-    <TopContainer>
-      <div className="logo">
-        <img src="/logo.svg" alt="logo" />
-      </div>
-      <div className="search">
-        <input type="search" placeholder="Search Food" />
-      </div>
-    </TopContainer>
-    <FilterContainer>
-      <Button>All</Button>
-      <Button>BreakFast</Button>
-      <Button>Lunch</Button>
-      <Button>Dinner</Button>
-    </FilterContainer>
-    <FoodCardContainer>
-      <FoodCard></FoodCard>
-    </FoodCardContainer>
-</Container>
-
-  )
+    <Container>
+      <TopContainer>
+        <div className="logo">
+          <img src="/logo.svg" alt="logo" />
+        </div>
+        <div className="search">
+          <input type="search" placeholder="Search Food" />
+        </div>
+      </TopContainer>
+      <FilterContainer>
+        <Button>All</Button>
+        <Button>BreakFast</Button>
+        <Button>Lunch</Button>
+        <Button>Dinner</Button>
+      </FilterContainer>
+      <FoodCardContainer>
+        <FoodCard></FoodCard>
+      </FoodCardContainer>
+    </Container>
+  );
 };
 
 export default App;
 
 const Container = styled.div`
-max-width: 1200px;
-margin: 0 auto;
-`
+  max-width: 1200px;
+  margin: 0 auto;
+`;
 const TopContainer = styled.section`
   min-height: 120px;
   display: flex;
@@ -63,38 +62,36 @@ const TopContainer = styled.section`
   align-items: center;
   padding: 16px;
 
-  .search{
-    input{
+  .search {
+    input {
       background-color: transparent;
       border: 1px solid red;
       color: white;
       border-radius: 5px;
-      height:40px;
+      height: 40px;
       font-size: 16px;
       padding: 0 10px;
     }
   }
-`
+`;
 const FilterContainer = styled.section`
   display: flex;
   justify-content: center;
-  gap:12px;
-  padding-bottom:40px;
-`
+  gap: 12px;
+  padding-bottom: 40px;
+`;
 
 const Button = styled.button`
-background-color: #ff4343;
-border-radius:5px;
-padding: 6px 12px;
-border: none;
-color: white;
-`
+  background-color: #ff4343;
+  border-radius: 5px;
+  padding: 6px 12px;
+  border: none;
+  color: white;
+`;
 
 const FoodCardContainer = styled.section`
-height: calc(100vh -210px);
+  height: calc(100vh -210px);
   background-image: url("/bg.png");
-background-size: cover;
-`
-const FoodCard = styled.div`
-  
-`
+  background-size: cover;
+`;
+const FoodCard = styled.div``;
